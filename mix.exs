4570602e640db71d2b04f79dc7a3d8568e94a450
Jan van de Molengraft <jan@artemisc.eu>
2017-07-04 20:55:20 +0200

    version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Salty.Mixfile do
   def project do
     [
       app: :salty,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
