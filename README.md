76af4e120109ee2b4b5dffd89c17804f9f59b1e9
Jan van de Molengraft <jan@artemisc.eu>
2017-07-04 19:10:46 +0200

    license/package update

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # Salty
 
-Salty provides bindings to [libsodium](https://github.com/jedisct1/libsodium)
-for Elixir, based on Nifs.
+Libsalty provides bindings to [libsodium](https://github.com/jedisct1/libsodium)
+for Elixir, based on [Erlang nif](http://erlang.org/doc/man/erl_nif.html).
 
-_This is a Work In Progress, bindings and API are incomplete and unstable_
+credits to [**Libsodium**](https://github.com/jedisct1/libsodium): author [Frank Denis](https://github.com/jedisct1)
+and [Contributors](https://github.com/jedisct1/libsodium/graphs/contributors).
+Libsodium is licensed under [ISC License](https://github.com/jedisct1/libsodium/blob/master/LICENSE)
 
-credits to [**Libsodium**](https://github.com/jedisct1/libsodium): author [Frank Denis](https://github.com/jedisct1) and [Contributors](https://github.com/jedisct1/libsodium/graphs/contributors).  Libsodium is licensed under [ISC License](https://github.com/jedisct1/libsodium/blob/master/LICENSE)
+## License
 
-This work is licensed under Apache 2.0
+Copyright 2017 Project ArteMisc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- mix.exs
+++ mix.exs
@@ -1,18 +1,59 @@
 defmodule Salty.Mixfile do
   use Mix.Project
 
+  @github "https://github.com/ArteMisc/libsalty"
+
   def project do
-    [app: :salty,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: []]
+    [
+      app: :salty,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: [readme: true, main: "README.md"],
+      source_url: @github
+    ]
   end
 
   # Configuration for the OTP application
   def application do
-    [extra_applications: [:logger],
-     mod: {Salty.Application, []}]
+    [mod: {Salty.Application, []}]
+  end
+
+  defp aliases do
+    [compile: ["compile", &make/1]]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp make(_) do
+    IO.puts "compiling salty_nif bindings"
+    unless Mix.shell.cmd("make") === 0 do
+      raise Mix.Error, message: "make encountered an error"
+    end
+    IO.puts "compiling salty_nif bindings done"
+  end
+
+  defp description do
+    """
+    An Elixir wrapper around the libsodium cryptographic library.
+    Based on erlang-nif.
+    """
+  end
+
+  defp package do
+    [
+      name: "Libsalty",
+      files: ["config", "src", "lib", "mix.exs", "Makefile", "LICENSE*", "README*"],
+      maintainers: ["jan@artemisc.eu"],
+      licenses: ["Apacha License, Version 2.0"],
+      links: %{"Github" => @github}
+    ]
   end
 end
